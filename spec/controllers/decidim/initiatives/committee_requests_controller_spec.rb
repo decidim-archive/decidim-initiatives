0e7ff48989ed70cf469b3cff546962d89573b556
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2017-12-18 09:19:37 +0100

    Rebase errors
    Fixed errors after rabasing with master.
    Fixed rubocop offenses.

--- spec/controllers/decidim/initiatives/committee_requests_controller_spec.rb
+++ spec/controllers/decidim/initiatives/committee_requests_controller_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require "spec_helper"
 
 module Decidim
   module Initiatives
@@ -11,24 +11,23 @@ module Decidim
       let!(:initiative) { create(:initiative, :created, organization: organization) }
 
       before do
-        @request.env['decidim.current_organization'] = organization
+        @request.env["decidim.current_organization"] = organization
       end
 
-      context 'GET new' do
-        context 'Owner requests membership' do
+      context "when GET new" do
+        context "with owner requests membership" do
           before do
             sign_in initiative.author
           end
 
-          it 'Owner is not allowed to request membership' do
+          it "Owner is not allowed to request membership" do
             get :new, params: { initiative_slug: initiative.slug }
             expect(flash[:alert]).not_to be_empty
             expect(response).to have_http_status(302)
           end
-
         end
 
-        context 'Authorized user' do
+        context "with authorized user" do
           let(:user) { create(:user, :confirmed, organization: organization) }
 
           before do
@@ -36,22 +35,21 @@ module Decidim
             sign_in user
           end
 
-          it 'Authorized users are allowed to request membership' do
+          it "are allowed to request membership" do
             get :new, params: { initiative_slug: initiative.slug }
             expect(flash[:alert]).to be_blank
             expect(response).to have_http_status(200)
           end
         end
 
-        context 'Unauthorized users do' do
+        context "with unauthorized users do" do
           let(:user) { create(:user, :confirmed, organization: organization) }
 
           before do
-            user.authorizations.delete_all
             sign_in user
           end
 
-          it 'are not allowed to request membership' do
+          it "are not allowed to request membership" do
             get :new, params: { initiative_slug: initiative.slug }
             expect(flash[:alert]).not_to be_empty
             expect(response).to have_http_status(302)
@@ -59,7 +57,7 @@ module Decidim
         end
       end
 
-      context 'GET spawn' do
+      context "when GET spawn" do
         let(:user) { create(:user, :confirmed, organization: organization) }
 
         before do
@@ -67,14 +65,14 @@ module Decidim
           sign_in user
         end
 
-        context 'created initiative' do
-          it 'Membership request is created' do
+        context "and created initiative" do
+          it "Membership request is created" do
             expect do
               get :spawn, params: { initiative_slug: initiative.slug }
             end.to change { InitiativesCommitteeMember.count }.by(1)
           end
 
-          it 'Duplicated requests finish with an error' do
+          it "Duplicated requests finish with an error" do
             expect do
               get :spawn, params: { initiative_slug: initiative.slug }
             end.to change { InitiativesCommitteeMember.count }.by(1)
@@ -85,10 +83,10 @@ module Decidim
           end
         end
 
-        context 'published initiative' do
+        context "and published initiative" do
           let!(:published_initiative) { create(:initiative) }
 
-          it 'Membership request is not created' do
+          it "Membership request is not created" do
             expect do
               get :spawn, params: { initiative_slug: published_initiative.slug }
             end.to change { InitiativesCommitteeMember.count }.by(0)
