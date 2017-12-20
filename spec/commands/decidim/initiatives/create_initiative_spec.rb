3415e32a8479e755aeff7d7c857ddd1025db684d
Oriol Gual <oriol.gual@gmail.com>
2017-12-20 15:25:36 +0100

    Notify author's followers on initiative creation

--- spec/commands/decidim/initiatives/create_initiative_spec.rb
+++ spec/commands/decidim/initiatives/create_initiative_spec.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require "spec_helper"
 
 module Decidim
   module Initiatives
     describe CreateInitiative do
       let(:form_klass) { InitiativeForm }
 
-      context 'Happy path' do
-        it_behaves_like 'create an initiative'
+      context "Happy path" do
+        it_behaves_like "create an initiative"
       end
 
-      context 'invalid data' do
+      context "invalid data" do
         let(:organization) { create(:organization) }
         let(:initiative) { create(:initiative, organization: organization) }
         let(:form) {
@@ -24,11 +24,48 @@ module Decidim
 
         let(:command) { described_class.new(form, initiative.author) }
 
-        it 'broadcasts invalid' do
+        it "broadcasts invalid" do
           expect_any_instance_of(Initiative).to receive(:persisted?)
                                                   .at_least(:once)
                                                   .and_return(false)
           expect { command.call }.to broadcast :invalid
+        end
+      end
+
+      describe "events" do
+        subject do
+          described_class.new(form, author)
+        end
+
+        let(:scoped_type) { create(:initiatives_type_scope) }
+        let(:organization) { scoped_type.type.organization }
+        let(:author) { create(:user, organization: organization) }
+        let(:form) { form_klass.from_params(form_params).with_context(current_organization: organization) }
+        let(:form_params) do
+          {
+            title: 'A reasonable initiative title',
+            description: 'A reasonable initiative description',
+            type_id: scoped_type.type.id,
+            signature_type: 'online',
+            scope_id: scoped_type.scope.id,
+            decidim_user_group_id: nil
+          }
+        end
+        let(:author) { create(:user, organization: organization) }
+        let(:follower) { create(:user, organization: organization) }
+        let!(:follow) { create :follow, followable: author, user: follower }
+
+        it "notifies the creation" do
+          expect(Decidim::EventsManager)
+            .to receive(:publish)
+            .with(
+            event: "decidim.events.initiatives.initiative_created",
+            event_class: Decidim::Initiatives::CreateInitiativeEvent,
+            resource: kind_of(Decidim::Initiative),
+            recipient_ids: [follower.id]
+          )
+
+          subject.call
         end
       end
     end
