4fb243fbaedd8b1f04a26745d4d0d1a417f56f17
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2018-01-12 05:44:40 +0100

    Fixes #100
    attendees_count will fallback to zero in case it's nil.

--- app/presenters/decidim/initiatives/initiative_stats_presenter.rb
+++ app/presenters/decidim/initiatives/initiative_stats_presenter.rb
@@ -13,7 +13,8 @@ module Decidim
       def comments_count
         Rails.cache.fetch(
           "initiative/#{initiative.id}/comments_count",
-          expires_in: Decidim::Initiatives.stats_cache_expiration_time) do
+          expires_in: Decidim::Initiatives.stats_cache_expiration_time
+        ) do
           Decidim::Comments::Comment.where(root_commentable: initiative).count
         end
       end
@@ -21,7 +22,8 @@ module Decidim
       def meetings_count
         Rails.cache.fetch(
           "initiative/#{initiative.id}/meetings_count",
-          expires_in: Decidim::Initiatives.stats_cache_expiration_time) do
+          expires_in: Decidim::Initiatives.stats_cache_expiration_time
+        ) do
           Decidim::Meetings::Meeting.where(feature: meetings_feature).count
         end
       end
@@ -29,10 +31,11 @@ module Decidim
       def assistants_count
         Rails.cache.fetch(
           "initiative/#{initiative.id}/assistants_count",
-          expires_in: Decidim::Initiatives.stats_cache_expiration_time) do
+          expires_in: Decidim::Initiatives.stats_cache_expiration_time
+        ) do
           result = 0
           Decidim::Meetings::Meeting.where(feature: meetings_feature).each do |meeting|
-            result += meeting.attendees_count
+            result += meeting.attendees_count || 0
           end
 
           result
@@ -42,7 +45,7 @@ module Decidim
       private
 
       def meetings_feature
-        @meetings_feature ||= Decidim::Feature.find_by(participatory_space: initiative, manifest_name: 'meetings')
+        @meetings_feature ||= Decidim::Feature.find_by(participatory_space: initiative, manifest_name: "meetings")
       end
     end
   end
